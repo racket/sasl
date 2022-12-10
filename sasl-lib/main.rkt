48dce713f7161421f5bb6b44c08f0fa9c0b7eae3
Bogdan Popa <bogdan@defn.io>
2022-12-10 13:24:19 +0200

    core: add make-sasl-ctx
    Closes #3

--- sasl-lib/main.rkt
+++ sasl-lib/main.rkt
@@ -2,8 +2,15 @@
 (require racket/contract/base
          "private/base.rkt")
 (provide sasl-ctx?
+         sasl-next/c
          (contract-out
+          [make-sasl-ctx        (-> any/c (or/c #f bytes? string?) sasl-next/c sasl-ctx?)]
           [sasl-next-message    (-> sasl-ctx? any/c)]
           [sasl-receive-message (-> sasl-ctx? any/c any)]
           [sasl-state           (-> sasl-ctx? symbol?)])
          (struct-out exn:fail:sasl:fatal))
+
+(define sasl-next/c
+  (or/c 'done (-> any/c (or/c bytes? string?)
+                  (values (or/c #f bytes? string?)
+                          (recursive-contract sasl-next/c)))))
